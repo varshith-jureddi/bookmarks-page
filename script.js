cb9895c7137bd621dbddcaf3baf366d1a27c2c01
varshith-jureddi <jureddi.varshith@gmail.com>
2026-06-02 20:08:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const addBookmarkBtn = document.getElementById("add");
+const bookmarkList = document.getElementById("list");
+const bookmarkNameInput = document.getElementById("bname");
+const bookmarkUrlInput = document.getElementById("blink");
+
+document.addEventListener("DOMContentLoaded", loadBookmarks);
+
+addBookmarkBtn.addEventListener("click", function () {
+  const name = bookmarkNameInput.value.trim();
+  const url = bookmarkUrlInput.value.trim();
+
+  if (!name || !url) {
+    alert("Please enter both name and URL.");
+    return;
+  } else {
+    // if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    //   alert("Please enter a valid URL starting with http:// or https://");
+    //   return;
+    // }
+
+    addBookmark(name, url);
+    saveBookmark(name, url);
+    bookmarkNameInput.value = "";
+    bookmarkUrlInput.value = "";
+  }
+});
+
+function addBookmark(name, url) {
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = url;
+  link.textContent = name;
+  link.target = "_blank";
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.addEventListener("click", function () {
+    bookmarkList.removeChild(li);
+    removeBookmarkFromStorage(name, url);
+  });
+
+  li.appendChild(link);
+  li.appendChild(removeButton);
+
+  bookmarkList.appendChild(li);
+}
+
+function getBookmarksFromStorage() {
+  const bookmarks = localStorage.getItem("bookmarks");
+  return bookmarks ? JSON.parse(bookmarks) : [];
+}
+
+function saveBookmark(name, url) {
+  const bookmarks = getBookmarksFromStorage();
+  bookmarks.push({ name, url });
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+//   ("bookmark added")
+}
+
+function loadBookmarks() {
+  const bookmarks = getBookmarksFromStorage();
+  bookmarks.forEach((bookmark) => addBookmark(bookmark.name, bookmark.url));
+}
+
+function removeBookmarkFromStorage(name, url) {
+  let bookmarks = getBookmarksFromStorage();
+  bookmarks = bookmarks.filter((bookmark) => bookmark.name !== name || bookmark.url !== url);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
